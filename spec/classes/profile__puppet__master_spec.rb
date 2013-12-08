b3bf86d02c3fe3baae93dc01279927a223a989cb
Lucas Bickel <hairmare@purplehaze.ch>
2013-12-08 23:58:25 +0100

    check for puppet::couch module

--- spec/classes/profile__puppet__master_spec.rb
+++ spec/classes/profile__puppet__master_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe 'profile::puppet::master' do
   it {
     should contain_class('puppet')
+    should contain_class('puppet::couch').that_requires('Class[puppet]').with_url('http://puppet-couch.example.com:5984/puppet')
   }
 end
