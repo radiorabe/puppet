b93c3b0ea4aedf46351c3e0a3fc7d021999fe140
Lucas Bickel <hairmare@purplehaze.ch>
2013-12-08 23:54:25 +0100

    insrtall puppet agent in master profile

---
+++ profile/manifests/puppet/master.pp
@@ -0,0 +1,11 @@
+# = Class: profile::puppet::master
+#
+# Set up a puppet master that uses couch for assigning profiles and configuration data.
+# This master also uses jabber for reporting to a muc.
+#
+class profile::puppet::master {
+
+  class { 'puppet':
+    master  => hiera('puppet_master')
+  }
+}
