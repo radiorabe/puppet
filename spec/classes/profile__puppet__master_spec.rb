732553c12ecd11c83dbd1e8d181fdc634b7ef457
Lucas Bickel <hairmare@purplehaze.ch>
2013-12-09 00:24:12 +0100

    check for master param on puppet class

--- spec/classes/profile__puppet__master_spec.rb
+++ spec/classes/profile__puppet__master_spec.rb
@@ -12,7 +12,7 @@ describe 'profile::puppet::master' do
         :xmpp_target   => 'puppet@conference.jabber.example.com'
       }
     ).that_comes_before('Class[puppet::master]')
-    should contain_class('puppet')
+    should contain_class('puppet').with_master('puppet-master.example.com')
     should contain_class('puppet::couch').that_requires('Class[puppet]').with_url('http://puppet-couch.example.com:5984/puppet')
     should contain_class('puppet::storeconfig').that_requires('Class[puppet]')
     should contain_class('puppet::master').that_requires('Class[puppet]')
