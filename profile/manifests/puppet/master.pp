32179c7ef5a1c279c5a62381127d5070017e02ed
Lucas Bickel <hairmare@purplehaze.ch>
2013-12-09 00:31:12 +0100

    some docs documenting the profile

--- profile/manifests/puppet/master.pp
+++ profile/manifests/puppet/master.pp
@@ -3,6 +3,23 @@
 # Set up a puppet master that uses couch for assigning profiles and configuration data.
 # This master also uses jabber for reporting to a muc.
 #
+# Due to the age of the puppet module this profile needs some configuration in the global
+# scope. Please return these global params from your ENC.
+#
+# ````
+#   "puppet_server": "puppet-master.example.com",
+#   "puppet_master": "true",
+#   "puppet_storeconfig_provider": "activerecord",
+#   "puppet_storeconfig_db_adapter": "mysql",
+#   "puppet_storeconfig_db_user": "root",
+#   "puppet_storeconfig_db_password": "",
+#   "puppet_storeconfig_db_server": "puppet-mysql.example.com",
+#   "puppet_storeconfig_db_name": "puppet",
+#   "puppet_pluginsync": "true"
+# ````
+#
+# You might also consider refactoring the puppet module to get rid of these.
+#
 class profile::puppet::master {
 
   class {
